84885fb8be7d43748cd14083b73edd3c4f4a8eb1
tkmoney11 <tylerkim@outlook.com>
2021-02-15 12:15:48 -0800

    Added v0 user entity

---
+++ src/main/java/com/tkapps/social/service/user/model/User.java
@@ -0,0 +1,23 @@
+package com.tkapps.social.service.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String email;
+}
